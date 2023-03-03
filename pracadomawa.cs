c743d75eac057bb8ee42991ee91276c8353d48b3
bartekkania499 <116822500+bartekkania499@users.noreply.github.com>
2023-03-03 08:22:04 +0100

    Create pracadomawa.cs

---
+++ pracadomawa.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 100;
+            double x2 = 13.2323;
+            float x3 = 123.123123F;
+            decimal x4 = 144.1444m;
+            byte x5 = 155;
+            short x6 = 32;
+            int x7 = 255;
+            long x8 = 456;
+            char x9 = "15";
+            bool x10 = true;
+
+            Console.WriteLine(x2);
+            Console.WriteLine(x);
+            Console.WriteLine(x3);
+            Console.WriteLine(x4);
+            Console.WriteLine(x5);
+            Console.WriteLine(x6);
+            Console.WriteLine(x7);
+            Console.WriteLine(x8);
+            Console.WriteLine(x9);
+            Console.WriteLine(x10);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
